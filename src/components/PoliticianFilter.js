77968004d8da4be58e15f1a8bb8b6122e96cd1ea
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-04-16 15:43:35 -0400

    Basic layout with semantic

--- src/components/PoliticianFilter.js
+++ src/components/PoliticianFilter.js
@@ -1,6 +1,17 @@
 function PoliticianFilter() {
     return (
-        <h1>Politician Filter</h1>
+        <div className="filter">
+            <select className="filter-dropdown" id="party">
+                <option value="All">Party</option>
+                <option value="Republican">Republican</option>
+                <option value="Democrat">Democrat</option>
+                <option value="Libertarian">Libertarian</option>
+            </select>
+            <select className="filter-dropdown" id="state">
+                <option value="All">State</option>
+                <option value="Georgia">GA</option>
+            </select>
+        </div>
     )
 }
 

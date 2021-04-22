437bb8e81e583d7fb6fac1b9b4f6a1720a22629c
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-04-22 09:29:58 -0400

    Splash page added

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react"
 import Search from "./Search"
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 
 function SideBar() {
     const [politiciansArray, setPoliticiansArray] = useState([])
@@ -15,13 +15,17 @@ function SideBar() {
 
     return (
         <div className="sidebar" id="side-bar-sticky">
-            <img src="https://i.imgur.com/mEwNslf.png" style={{ width : 225 }} alt="logo" />
+            <Link to="/">
+                <img src="https://i.imgur.com/mEwNslf.png" style={{ width : 225 }} alt="logo" />
+            </Link>
             <Search politiciansArray={politiciansArray}/>
-            <nav>
-                <NavLink className="sidbar-btn" to="/">Watchlist</NavLink>
-                <br/>
-                <NavLink className="sidbar-btn" to="/politicians">Politicians</NavLink>
-            </nav>
+            <div>
+                <nav>
+                    <NavLink className="sidbar-btn" to="/watchlist">Watchlist</NavLink>
+                    <br/>
+                    <NavLink className="sidbar-btn" to="/politicians">Politicians</NavLink>
+                </nav>
+            </div>
         </div>
     )
 }

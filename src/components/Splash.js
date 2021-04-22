2d9d93066202279b9ac6a589e275fb02ea0428ef
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-04-22 09:57:44 -0400

    tiny tweek to splash

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,7 +3,7 @@ function Splash() {
         <div className="main-page" id="splash">
             <img src="https://i.imgur.com/NHl7oRO.png" alt="toto-banner" style={{ width: 600}}/>
             <div id="splash-blurb">
-                <h3>Welcome to <span>toto.</span></h3>
+                <h3>Welcome to <span>toto</span>.</h3>
                 <p>In politics, the quickest path to the truth leads straight through the money.</p>
                 <p>We make that path easier to follow.</p>
             </div>
